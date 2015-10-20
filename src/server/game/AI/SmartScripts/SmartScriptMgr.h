c7d426d22f0e2462459380dc7572745dd9b0f63a
r00ty-tc <r00ty-tc@users.noreply.github.com>
2015-10-20 01:35:35 +0100

    Dynamic Creature/Go spawning:
    * Don't load objects during grid load, if they have a respawn timer
    * Load from respawn DB into grid respawn lists
    * re-write Creature Respawn() function to work with dynamic spawns
    * Create separate function to respawn from list
    * Created helper functions to remove/respawn based on spawnid, gridid,
      cell/area/zone
    * re-write .respawn command to use new functionality.
    * Removed existing respawn removal functions in favour of new versions
      aside from "compatibility mode"
    * Compatibility mode added for dynamic spawns.
    * Extended .gobject info command with more useful data. GUID, location and
      distance.
    
    Rewrite of storage mechanism for respawns:
    
    * Now uses 3 maps, for cell/zone/area + grid + spawnID for multiple lookups
    * Single respawnInfo object and pointers in each map to the same entry
    * Helper functions to ensure no dead pointers, no storage left unattached
    etc.
    * A compatibility mode added for creatures for interim use until BG/BF
    and instances are converted.
    
    * temporary summons/non DB spawns will by default use the old spawn system
      with an option to use the new one. Meaning they need no changes to still
      function Meaning they need no changes to still function.
      Marked as "Compatibility mode"
    * Dynamic spawns will only be active by default on DB loaded creatures.
    
    Creature/Go group functionaity:
    * New tables added for group template/membership
    * Load creatures/gos into groups (default 0)
    * Default (system) groups 0-255, 256+ user groups (manual spawn only on
      user groups)
    * Honour flag for compatibility mode (don't use dynamic spawning if group
      has this flag set)
    * Fully implement compatibility mode for both creature and gos
    * Implement display for group and compatibility mode state for creature
      and go with npc info and gobject info commands
    * c++ functions to Spawn/Despawn manual creature/go groups
    * new commands gobject spawngroup/despawngroup and npc
      spawngroup/despawngroup to spawn/despawn manual groups for testing
    * SAI actions to spawn/despawn creature/go groups
    * Extended functionality to allow forced spawn, ignore respawn and remove
      respawn time on despawn. Functinality as follows:
     - Force spawn (will spawn all creatures/gameobjects in a group even if
       there are already active versions alive/active)
     - Ignore Repsawn (will spawn creatures in a group regardless of their
       respawn timer status)
     - removerespawntime (Despawn option, which will cause the respawn
       time to be removed when despawning group)
    
    Escort NPC changes:
    * Respawn timer starts when an escort quest is taken, not on creature
      death
    * Initial support for respawning replacement escort NPCs when user takes
      quest
    * General protection against respawning if any spawned instances of the
      spawnId are not currently on escort.
    * Limited to creatures with the correct escort NPC flag
    * Supports dynamic respawn rates
    
    Dynamic Respawn Rates:
    * Based on player within dynamic spawn scope. Cell (specified radius),
      Zone or Area.
    * Will decrease respawn time based on a factor multiplied by number of
      players within scope range. With minimum respawn time.
    * Optionally applied to Escort NPCs.
    
    Auto server restart:
    * Will queue a restart at a convenient time when one threshold is
      breached. With regular warnings and a 30 minute countdown when the time
      comes.
    * When a second threshold is reached an emergency 5 minute restart is
      queued.
    
    Misc changes:
    * Support for grid persistance (no grid unload for wintergrasp during
      battle for example)
    
    Entity/Creature and DB/Conditions: Implement new CONDITION_SOURCE_TYPE_CREATURE.
    Author: Treeston
    
    * CONDITION_SOURCE_TYPE_CREATURE can be used to restrict spawning of creature (SourceEntry = creature GUID)
    * Add conditions to all of Naxxramas and ICC. Trash will now properly not respawn if the respective boss is killed, and will also properly despawn on soft reset after the boss is dead. Fixes and closes #15682.

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -551,8 +551,12 @@ enum SMART_ACTION
     SMART_ACTION_START_CLOSEST_WAYPOINT             = 113,    // wp1, wp2, wp3, wp4, wp5, wp6, wp7
     SMART_ACTION_RISE_UP                            = 114,    // distance
     SMART_ACTION_RANDOM_SOUND                       = 115,    // soundId1, soundId2, soundId3, soundId4, soundId5, onlySelf
+    SMART_ACTION_SPAWN_CREATUREGROUP                = 116,    // Group ID, min secs, max secs
+    SMART_ACTION_DESPAWN_CREATUREGROUP              = 117,    // Group ID, min secs, max secs
+    SMART_ACTION_SPAWN_GAMEOBJECTGROUP              = 118,    // Group ID, min secs, max secs
+    SMART_ACTION_DESPAWN_GAMEOBJECTGROUP            = 119,    // Group ID, min secs, max secs
 
-    SMART_ACTION_END                                = 116
+    SMART_ACTION_END                                = 120
 };
 
 struct SmartAction
@@ -1034,6 +1038,14 @@ struct SmartAction
             uint32 onlySelf;
         } randomSound;
 
+        struct
+        {
+            uint32 groupId;
+            uint32 minDelay;
+            uint32 maxDelay;
+            uint32 spawnflags;
+        } groupSpawn;
+
         //! Note for any new future actions
         //! All parameters must have type uint32
 
@@ -1047,6 +1059,14 @@ struct SmartAction
             uint32 param6;
         } raw;
     };
+};
+
+enum SMARTAI_SPAWN_FLAGS
+{
+    SMARTAI_SPAWN_FLAG_NONE                 = 0x00,
+    SMARTAI_SPAWN_FLAG_IGNORE_RESPAWN       = 0x01,
+    SMARTAI_SPAWN_FLAG_FORCE_SPAWN          = 0x02,
+    SMARTAI_SPAWN_FLAG_NOSAVE_RESPAWN       = 0x04,
 };
 
 enum SMARTAI_TEMPLATE
